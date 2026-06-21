d32573c2fdcd4c82b7384d62ef4e255c223c6430
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 03:02:50 +0900

    design : 예약 확정 모달 마크업 (#21)

--- src/pages/main-page/components/confirm-modal/ConfirmModal.jsx
+++ src/pages/main-page/components/confirm-modal/ConfirmModal.jsx
@@ -1,5 +1,12 @@
 import './ConfirmModal.css'
 
 export default function ConfirmModal() {
-  return null
+  return (
+    <div className='confirm-modal__overlay'>
+      <div className='confirm-modal'>
+        <div className='confirm-modal__title'>예약이 확정되었습니다</div>
+        <p className='confirm-modal__desc'>예약정보와 취소는 마이페이지에서 가능합니다</p>
+      </div>
+    </div>
+  )
 }
