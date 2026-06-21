4e3bdd3a205e8cef31336515b8d4661f1aacfe45
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 02:30:07 +0900

    design : 상단 필터 바 마크업 (#18)

--- src/pages/main-page/components/filter-bar/FilterBar.jsx
+++ src/pages/main-page/components/filter-bar/FilterBar.jsx
@@ -1,5 +1,22 @@
 import './FilterBar.css'
 
 export default function FilterBar() {
-  return null
+  return (
+    <div className='filter-bar'>
+      <div className='filter-bar__toggle'>
+        <div className='filter-bar__toggle-calendar' />
+        <span className='filter-bar__toggle-text'>06/16/26</span>
+      </div>
+
+      <div className='filter-bar__toggle'>
+        <span className='filter-bar__toggle-text'>시간</span>
+        <span className='filter-bar__toggle-icon' />
+      </div>
+
+      <div className='filter-bar__toggle'>
+        <span className='filter-bar__toggle-text'>1층</span>
+        <span className='filter-bar__toggle-icon' />
+      </div>
+    </div>
+  )
 }
