42e9b17b78ee0a0c6f0f8eb0bc874dc856dd91e1
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 02:51:06 +0900

    design : 플로어맵 마크업 (#19)

--- src/pages/main-page/components/floor-map/FloorMap.jsx
+++ src/pages/main-page/components/floor-map/FloorMap.jsx
@@ -1,5 +1,12 @@
 import './FloorMap.css'
 
 export default function FloorMap() {
-  return null
+  return (
+    <div className='floor-map'>
+      <div className='floor-map__board'>
+        {/* SVG 도면 위에 강의실 버튼을 절대 위치로 올리는 구조 */}
+        {/* SVG 파일 추가 후 작업 예정 */}
+      </div>
+    </div>
+  )
 }
