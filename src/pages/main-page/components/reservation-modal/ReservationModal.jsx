57a339e937cf87612e4d5dac1f147b9b9bb65381
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 02:56:57 +0900

    design : 예약 신청 모달 마크업 (#20)

--- src/pages/main-page/components/reservation-modal/ReservationModal.jsx
+++ src/pages/main-page/components/reservation-modal/ReservationModal.jsx
@@ -1,5 +1,34 @@
 import './ReservationModal.css'
 
 export default function ReservationModal() {
-  return null
+  return (
+    <div className='reservation-modal__overlay'>
+      <div className='reservation-modal'>
+        <div className='reservation-modal__title'>예약정보</div>
+
+        <div className='reservation-modal__info'>
+          <div className='reservation-modal__info-item'>
+            <span className='reservation-modal__info-label'>날짜</span>
+            <span className='reservation-modal__info-value'>4/1</span>
+          </div>
+          <div className='reservation-modal__info-item'>
+            <span className='reservation-modal__info-label'>강의실</span>
+            <span className='reservation-modal__info-value'>101호</span>
+          </div>
+          <div className='reservation-modal__info-item'>
+            <span className='reservation-modal__info-label'>예약자 정보</span>
+            <span className='reservation-modal__info-value'>2024122243</span>
+          </div>
+          <div className='reservation-modal__info-item'>
+            <span className='reservation-modal__info-label'>시간</span>
+            <span className='reservation-modal__info-value'>18~19</span>
+          </div>
+        </div>
+
+        <button type='button' className='reservation-modal__btn'>
+          예약신청
+        </button>
+      </div>
+    </div>
+  )
 }
