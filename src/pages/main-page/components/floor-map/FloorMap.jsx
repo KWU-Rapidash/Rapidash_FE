caa0649a2358e4a7b5a69018b0968a04d3b5d50f
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 02:51:06 +0900

    design : 플로어맵 마크업 (#19)

--- src/pages/main-page/components/floor-map/FloorMap.jsx
+++ src/pages/main-page/components/floor-map/FloorMap.jsx
@@ -16,6 +16,17 @@ export default function FloorMap({ floor = 1 }) {
 =======
 
 export default function FloorMap() {
+<<<<<<< HEAD
   return null
 >>>>>>> 869d6b7 (design : 메인 페이지 레이아웃 마크업 및 라우터 등록 (#27))
+=======
+  return (
+    <div className='floor-map'>
+      <div className='floor-map__board'>
+        {/* SVG 도면 위에 강의실 버튼을 절대 위치로 올리는 구조 */}
+        {/* SVG 파일 추가 후 작업 예정 */}
+      </div>
+    </div>
+  )
+>>>>>>> 42e9b17 (design : 플로어맵 마크업 (#19))
 }
