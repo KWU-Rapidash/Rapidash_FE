f1d255a5b042eefee1c8437c07538446f00637c7
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 03:39:51 +0900

    design : 필터 바 아이콘 적용 (#23)

--- src/pages/main-page/components/filter-bar/FilterBar.jsx
+++ src/pages/main-page/components/filter-bar/FilterBar.jsx
@@ -1,21 +1,23 @@
 import './FilterBar.css'
+import CalendarIcon from '../../../../assets/filter-bar/calendar-icon.svg'
+import DropdownIcon from '../../../../assets/filter-bar/dropdown-icon.svg'
 
 export default function FilterBar() {
   return (
     <div className='filter-bar'>
       <div className='filter-bar__toggle'>
-        <div className='filter-bar__toggle-calendar' />
+        <img src={CalendarIcon} alt='캘린더' className='filter-bar__toggle-calendar' />
         <span className='filter-bar__toggle-text'>06/16/26</span>
       </div>
 
       <div className='filter-bar__toggle'>
         <span className='filter-bar__toggle-text'>시간</span>
-        <span className='filter-bar__toggle-icon' />
+        <img src={DropdownIcon} alt='' className='filter-bar__toggle-icon' />
       </div>
 
       <div className='filter-bar__toggle'>
         <span className='filter-bar__toggle-text'>1층</span>
-        <span className='filter-bar__toggle-icon' />
+        <img src={DropdownIcon} alt='' className='filter-bar__toggle-icon' />
       </div>
     </div>
   )
