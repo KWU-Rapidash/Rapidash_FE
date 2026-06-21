259d3cc6990df22e1700e3c7066b165c24b3fa5b
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 03:56:26 +0900

    design : 플로어맵 SVG 이미지 추가 (#24)

--- src/pages/main-page/components/floor-map/FloorMap.jsx
+++ src/pages/main-page/components/floor-map/FloorMap.jsx
@@ -1,10 +1,14 @@
 import './FloorMap.css'
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> fb3ffa6 (design : 플로어맵 SVG 이미지 추가 (#24))
 import Floor1 from '../../../../assets/floor-map/floor-1.svg'
 import Floor2 from '../../../../assets/floor-map/floor-2.svg'
 
 export default function FloorMap({ floor = 1 }) {
   const floorSrc = floor === 1 ? Floor1 : Floor2
+<<<<<<< HEAD
 
   return (
     <div className='floor-map'>
@@ -20,11 +24,13 @@ export default function FloorMap() {
   return null
 >>>>>>> 869d6b7 (design : 메인 페이지 레이아웃 마크업 및 라우터 등록 (#27))
 =======
+=======
+
+>>>>>>> fb3ffa6 (design : 플로어맵 SVG 이미지 추가 (#24))
   return (
     <div className='floor-map'>
       <div className='floor-map__board'>
-        {/* SVG 도면 위에 강의실 버튼을 절대 위치로 올리는 구조 */}
-        {/* SVG 파일 추가 후 작업 예정 */}
+        <img src={floorSrc} alt={`${floor}층 플로어맵`} className='floor-map__image' />
       </div>
     </div>
   )
