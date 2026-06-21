bfbe7cf840e7dbc35733000e9b9b4598c7491b29
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 03:43:14 +0900

    design : 메인 페이지 에셋 적용 (#28)

--- src/pages/main-page/entry/MainPage.jsx
+++ src/pages/main-page/entry/MainPage.jsx
@@ -1,11 +1,16 @@
 import './MainPage.css'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import KwangwoonLogo from '../../../assets/header/kwangwoon-logo.svg'
 import MypageIcon from '../../../assets/header/mypage-icon.svg'
 =======
 import { ReactComponent as KwangwoonLogo } from '../../../assets/header/kwangwoon-logo.svg'
 import { ReactComponent as MypageIcon } from '../../../assets/header/mypage-icon.svg'
 >>>>>>> 8539197 (design : 메인 페이지 에셋 추가 및 import (#28))
+=======
+import KwangwoonLogo from '../../../assets/header/kwangwoon-logo.svg'
+import MypageIcon from '../../../assets/header/mypage-icon.svg'
+>>>>>>> 02871cc (design : 메인 페이지 에셋 적용 (#28))
 import FilterBar from '../components/filter-bar/FilterBar'
 import FloorMap from '../components/floor-map/FloorMap'
 import ReservationModal from '../components/reservation-modal/ReservationModal'
@@ -17,6 +22,7 @@ export default function MainPage() {
       <div className='main-page__topbar'>
         <div className='main-page__topbar-inner'>
 <<<<<<< HEAD
+<<<<<<< HEAD
           <img className='main-page__topbar-logo' src={KwangwoonLogo} alt='광운대학교' />
           <button className='main-page__topbar-mypage' type='button'>
             <img src={MypageIcon} alt='마이페이지' />
@@ -25,6 +31,11 @@ export default function MainPage() {
           <button className='main-page__topbar-mypage' type='button'>
             <MypageIcon />
 >>>>>>> 8539197 (design : 메인 페이지 에셋 추가 및 import (#28))
+=======
+          <img className='main-page__topbar-logo' src={KwangwoonLogo} alt='광운대학교' />
+          <button className='main-page__topbar-mypage' type='button'>
+            <img src={MypageIcon} alt='마이페이지' />
+>>>>>>> 02871cc (design : 메인 페이지 에셋 적용 (#28))
           </button>
         </div>
       </div>
