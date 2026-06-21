8539197c135c54968b50224f2ceb4d0964742e4b
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 03:24:35 +0900

    design : 메인 페이지 에셋 추가 및 import (#28)

--- src/pages/main-page/entry/MainPage.jsx
+++ src/pages/main-page/entry/MainPage.jsx
@@ -1,4 +1,6 @@
 import './MainPage.css'
+import { ReactComponent as KwangwoonLogo } from '../../../assets/header/kwangwoon-logo.svg'
+import { ReactComponent as MypageIcon } from '../../../assets/header/mypage-icon.svg'
 import FilterBar from '../components/filter-bar/FilterBar'
 import FloorMap from '../components/floor-map/FloorMap'
 import ReservationModal from '../components/reservation-modal/ReservationModal'
@@ -9,8 +11,10 @@ export default function MainPage() {
     <div className='main-page'>
       <div className='main-page__topbar'>
         <div className='main-page__topbar-inner'>
-          <img className='main-page__topbar-logo' alt='광운대학교' />
-          <button className='main-page__topbar-mypage' type='button' />
+          <KwangwoonLogo className='main-page__topbar-logo' />
+          <button className='main-page__topbar-mypage' type='button'>
+            <MypageIcon />
+          </button>
         </div>
       </div>
 
