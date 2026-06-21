113d7432544c4d99a2245f0febc2049493b2a8ec
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-22 05:18:33 +0900

    feat : 라우터 연결 (#27)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import './styles/global.css'
-import App from './App.jsx'
+import { AppRouter } from './components/AppRouter.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(<RouterProvider router={AppRouter} />)
