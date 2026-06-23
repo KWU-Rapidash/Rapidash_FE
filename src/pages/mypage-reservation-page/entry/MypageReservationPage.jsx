133b224ff47db8c61302b619079b2f7d3a8b4a92
eine0322 <eine113589@gmail.com>
2026-06-24 07:59:39 +0900

    feat : mypage reservation ui (#137)

--- src/pages/mypage-reservation-page/entry/MypageReservationPage.jsx
+++ src/pages/mypage-reservation-page/entry/MypageReservationPage.jsx
@@ -1,7 +1,30 @@
 import './MypageReservationPage.css'
+import ButtonBold from '../../../components/button-bold/ButtonBold'
 
 function MypageReservationPage() {
-  return <div>MypageReservationPage</div>
+  return (
+    <>
+      <div className='mypageReservation__container'>
+        <div className='mypageReservation__content'>
+          <div className='mypageReservation__label'>날짜</div>
+          <div className='mypageReservation__value'>4/1</div>
+        </div>
+        <div className='mypageReservation__content'>
+          <div className='mypageReservation__label'>강의실</div>
+          <div className='mypageReservation__value'>101호</div>
+        </div>
+        <div className='mypageReservation__content'>
+          <div className='mypageReservation__label'>예약자 정보</div>
+          <div className='mypageReservation__value'>2024122243 김나리</div>
+        </div>
+        <div className='mypageReservation__content'>
+          <div className='mypageReservation__label'>시간</div>
+          <div className='mypageReservation__value'>18~19</div>
+        </div>
+      </div>
+      <ButtonBold content='예약취소' />
+    </>
+  )
 }
 
 export default MypageReservationPage
