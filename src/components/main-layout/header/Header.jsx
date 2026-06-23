c27d9c86c00a037c35310cfcf6140adca9427bb4
eine0322 <eine113589@gmail.com>
2026-06-24 05:15:04 +0900

    feat : implement header mypage button

--- src/components/main-layout/header/Header.jsx
+++ src/components/main-layout/header/Header.jsx
@@ -9,13 +9,17 @@ function Header() {
   const isMypagePath =
     location.pathname === '/mypage' || location.pathname === '/mypage/reservation'
 
+  const isLoginPath = location.pathname === '/'
+
   return (
     <div className='header__container'>
       {isMypagePath && (
         <Icon name='back-arrow' onClick={() => navigate(-1)} width={32} height={32} />
       )}
       <Icon name='kwangwoon-logo' width={142} height={40} />
-      <Icon name='mypage-icon' onClick={() => navigate('/mypage')} width={24} height={24} />
+      {!isLoginPath && (
+        <Icon name='mypage-icon' onClick={() => navigate('/mypage')} width={24} height={24} />
+      )}
     </div>
   )
 }
