2f18fbb7505fefa17ae86a23e30f3772365dd2cc
eine0322 <eine113589@gmail.com>
2026-06-24 05:03:53 +0900

    feat : header js

--- src/components/main-layout/header/Header.jsx
+++ src/components/main-layout/header/Header.jsx
@@ -1,13 +1,19 @@
 import './Header.css'
 import { Icon } from '../../icon/Icon'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 function Header() {
   const navigate = useNavigate()
+  const location = useLocation()
+
+  const isMypagePath =
+    location.pathname === '/mypage' || location.pathname === '/mypage/reservation'
 
   return (
     <div className='header__container'>
-      <Icon name='back-arrow' onClick={() => navigate(-1)} width={32} height={32} />
+      {isMypagePath && (
+        <Icon name='back-arrow' onClick={() => navigate(-1)} width={32} height={32} />
+      )}
       <Icon name='kwangwoon-logo' width={142} height={40} />
       <Icon name='mypage-icon' onClick={() => navigate('/mypage')} width={24} height={24} />
     </div>
