50598be88af970648a9ec1ed7ccf8aaab1b046fe
김다은 <dddan0303@gmail.com>
2026-06-23 18:58:17 +0900

    feat : buttonBack 컴포넌트 props 연결 (#123)

--- src/components/button-back/ButtonBack.jsx
+++ src/components/button-back/ButtonBack.jsx
@@ -1,8 +1,19 @@
 import './ButtonBack.css'
 
-function ButtonBack() {
+function ButtonBack({
+  type = 'button',
+  ariaLabel = '뒤로가기',
+  onClick,
+  disabled = false,
+}) {
   return (
-    <button className="button-back" type="button" aria-label="뒤로가기">
+    <button
+      className="button-back"
+      type={type}
+      aria-label={ariaLabel}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <span className="button-back__icon" aria-hidden="true" />
     </button>
   )
