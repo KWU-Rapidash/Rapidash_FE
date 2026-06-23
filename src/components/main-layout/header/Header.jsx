732ddbdf7678f8e04a5d772cd8b4de696e1741e8
eine0322 <eine113589@gmail.com>
2026-06-24 04:48:48 +0900

    feat : header add navigate

--- src/components/main-layout/header/Header.jsx
+++ src/components/main-layout/header/Header.jsx
@@ -1,12 +1,15 @@
 import './Header.css'
 import { Icon } from '../../icon/Icon'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+  const navigate = useNavigate()
+
   return (
     <div className='header__container'>
-      <Icon name='back-arrow' width={32} height={32} />
+      <Icon name='back-arrow' onClick={() => navigate(-1)} width={32} height={32} />
       <Icon name='kwangwoon-logo' width={142} height={40} />
-      <Icon name='mypage-icon' width={24} height={24} />
+      <Icon name='mypage-icon' onClick={() => navigate('/mypage')} width={24} height={24} />
     </div>
   )
 }
