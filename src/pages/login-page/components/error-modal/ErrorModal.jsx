9e45a1709e0680c6257f2437c80659f3cedd02f3
eine0322 <eine113589@gmail.com>
2026-06-25 06:55:13 +0900

    feat : login errorModal UI, JS (#74) (#75)

--- src/pages/login-page/components/error-modal/ErrorModal.jsx
+++ src/pages/login-page/components/error-modal/ErrorModal.jsx
@@ -1,7 +1,22 @@
 import './ErrorModal.css'
 
 function ErrorModal({ onClose }) {
-  return <div onClick={onClose}>ErrorModal</div>
+  return (
+    <div className='errorModal__overlay' onClick={onClose}>
+      <div className='errorModal__container' onClick={(e) => e.stopPropagation()}>
+        <div className='errorModal__text--title cancelModal__text'>아이디/비밀번호 오류</div>
+        <div className='errorModal__text'>
+          <div className='errorModal__text--message'>
+            입력한 아이디 혹은 비밀번호가 올바르지 않습니다.
+          </div>
+          <div className='errorModal__text--message'>다시 시도해주세요.</div>
+        </div>
+        <div className='errorModal__button' onClick={onClose}>
+          확인
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ErrorModal
