b58a52bde7a4f6c54e84d7706ec28dde2f9d6b77
eine0322 <eine113589@gmail.com>
2026-06-25 03:33:23 +0900

    feat : mypageAccount Page js

--- src/components/button-bold/ButtonBold.jsx
+++ src/components/button-bold/ButtonBold.jsx
@@ -2,8 +2,10 @@ import './ButtonBold.css'
 
 function ButtonBold({ content, onClick }) {
   return (
-    <div className='buttonBold' onClick={onClick}>
-      {content}
+    <div className='buttonBold__container'>
+      <button className='buttonBold__button' onClick={onClick}>
+        {content}
+      </button>
     </div>
   )
 }
