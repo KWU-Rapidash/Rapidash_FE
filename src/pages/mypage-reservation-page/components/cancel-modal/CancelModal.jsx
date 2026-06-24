ceddc0e367319e9880fd3bf31349191970fcc679
eine0322 <eine113589@gmail.com>
2026-06-25 04:12:34 +0900

    feat : mypage cancleModal js

--- src/pages/mypage-reservation-page/components/cancel-modal/CancelModal.jsx
+++ src/pages/mypage-reservation-page/components/cancel-modal/CancelModal.jsx
@@ -1,4 +1,15 @@
+import './CancelModal.css'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+
 function CancelModal({ onClose }) {
+  const navigate = useNavigate()
+
+  const handleCancel = () => {
+    toast.success('예약이 취소되었습니다.')
+    navigate('/main')
+  }
+
   return (
     <div className='cancelModal__overlay'>
       <div className='cancelModal__container'>
@@ -10,7 +21,9 @@ function CancelModal({ onClose }) {
           <div className='cancelModal__button--back' onClick={onClose}>
             뒤로가기
           </div>
-          <div className='cancelModal__button--cancel'>예약취소</div>
+          <div className='cancelModal__button--cancel' onClick={handleCancel}>
+            예약취소
+          </div>
         </div>
       </div>
     </div>
