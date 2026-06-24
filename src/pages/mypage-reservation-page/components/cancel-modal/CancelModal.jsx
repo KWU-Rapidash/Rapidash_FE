f6d5cc7546ae2d67e7cf34991d4e65fe0d2eb3fc
eine0322 <eine113589@gmail.com>
2026-06-25 05:00:14 +0900

    design : mypage cancelModal css (#143)

--- src/pages/mypage-reservation-page/components/cancel-modal/CancelModal.jsx
+++ src/pages/mypage-reservation-page/components/cancel-modal/CancelModal.jsx
@@ -11,12 +11,10 @@ function CancelModal({ onClose }) {
   }
 
   return (
-    <div className='cancelModal__overlay'>
-      <div className='cancelModal__container'>
-        <div className='cancelModal__text'>
-          <div className='cancelModal__title'>예약취소</div>
-          <div className='cancelModal__message'>예약을 취소하시겠습니까?</div>
-        </div>
+    <div className='cancelModal__overlay' onClick={onClose}>
+      <div className='cancelModal__container' onClick={(e) => e.stopPropagation()}>
+        <div className='cancelModal__text--title cancelModal__text'>예약취소</div>
+        <div className='cancelModal__text--message cancelModal__text'>예약을 취소하시겠습니까?</div>
         <div className='cancelModal__button'>
           <div className='cancelModal__button--back' onClick={onClose}>
             뒤로가기
