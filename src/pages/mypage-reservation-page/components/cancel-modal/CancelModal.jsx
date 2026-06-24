174bf0741aef49f3b2ecd5bf79dc6e1877bdf5be
eine0322 <eine113589@gmail.com>
2026-06-25 04:03:27 +0900

    add : mypage cancleModal ui

--- src/pages/mypage-reservation-page/components/cancel-modal/CancelModal.jsx
+++ src/pages/mypage-reservation-page/components/cancel-modal/CancelModal.jsx
@@ -2,7 +2,16 @@ function CancelModal({ onClose }) {
   return (
     <div className='cancelModal__overlay'>
       <div className='cancelModal__container'>
-        Are you sure you want to cancel this reservation?
+        <div className='cancelModal__text'>
+          <div className='cancelModal__title'>예약취소</div>
+          <div className='cancelModal__message'>예약을 취소하시겠습니까?</div>
+        </div>
+        <div className='cancelModal__button'>
+          <div className='cancelModal__button--back' onClick={onClose}>
+            뒤로가기
+          </div>
+          <div className='cancelModal__button--cancel'>예약취소</div>
+        </div>
       </div>
     </div>
   )
