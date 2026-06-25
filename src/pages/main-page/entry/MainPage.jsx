0d0dd3ab8176715f1c2c1c948f0b6e0f07ad0066
leeeeeseojin <kkyuuu00@gmail.com>
2026-06-25 09:46:18 +0900

    fix : 메인 페이지 헤더 스타일 제거 (#202)

--- src/pages/main-page/entry/MainPage.jsx
+++ src/pages/main-page/entry/MainPage.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
 import './MainPage.css'
-import KwangwoonLogo from '../../../assets/header/kwangwoon-logo.svg'
-import MypageIcon from '../../../assets/header/mypage-icon.svg'
 import FilterBar from '../components/filter-bar/FilterBar'
 import FloorMap from '../components/floor-map/FloorMap'
 import ReservationModal from '../components/reservation-modal/ReservationModal'
@@ -51,15 +49,6 @@ export default function MainPage() {
 
   return (
     <div className='main-page'>
-      <div className='main-page__topbar'>
-        <div className='main-page__topbar-inner'>
-          <img className='main-page__topbar-logo' src={KwangwoonLogo} alt='광운대학교' />
-          <button className='main-page__topbar-mypage' type='button'>
-            <img src={MypageIcon} alt='마이페이지' />
-          </button>
-        </div>
-      </div>
-
       <div className='main-page__content'>
         <div className='main-page__filter'>
           <FilterBar
